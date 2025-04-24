5b66062307d1f5e92137bf0837f80a31825298d6
kwiremo <kwiremo@gmail.com>
2025-04-23 19:50:07 -0700

    Adds the booker logic

--- packages/platform/atoms/index.ts
+++ packages/platform/atoms/index.ts
@@ -5,3 +5,19 @@ export { SelectedCalendarsSettingsPlatformWrapper as SelectedCalendarsSettings }
 export { EventTypePlatformWrapper as EventTypeSettings } from "./event-types/wrappers/EventTypePlatformWrapper";
 export { CreateEventTypePlatformWrapper as CreateEventType } from "./event-types/wrappers/CreateEventTypePlatformWrapper";
 export { PaymentForm } from "./event-types/payments/PaymentForm";
+export { BookerPlatformWrapper as Booker } from "./booker/BookerPlatformWrapper";
+
+
+export { useCancelBooking } from "./hooks/bookings/useCancelBooking";
+export { useBooking } from "./hooks/bookings/useBooking";
+export { useBookings } from "./hooks/bookings/useBookings";
+
+export { PaymentForm } from "./event-types/payments/PaymentForm";
+export { EventTypePlatformWrapper as EventTypeSettings } from "./event-types/wrappers/EventTypePlatformWrapper";
+export { CreateEventTypePlatformWrapper as CreateEventType } from "./event-types/wrappers/CreateEventTypePlatformWrapper";
+export { useEventTypes } from "./hooks/event-types/public/useEventTypes";
+export { useTeamEventTypes } from "./hooks/event-types/public/useTeamEventTypes";
+export { useEventType as useEvent } from "./hooks/event-types/public/useEventType";
+export { useEventTypeById } from "./hooks/event-types/private/useEventTypeById";
+export { useCreateEventType } from "./hooks/event-types/private/useCreateEventType";
+export { useCreateTeamEventType } from "./hooks/event-types/private/useCreateTeamEventType";
